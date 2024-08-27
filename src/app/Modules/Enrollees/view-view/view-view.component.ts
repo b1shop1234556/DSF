31a9739a7cd4e82617439f60478db923c19e8d3b
Dionece12345 <160378010+Dionece12345@users.noreply.github.com>
2024-08-27 22:01:50 +0800

    uploadpage

--- src/app/Modules/Enrollees/view-view/view-view.component.ts
+++ src/app/Modules/Enrollees/view-view/view-view.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterModule } from '@angular/router';
+import { CustomSidenavComponent } from '../../../custom-sidenav/custom-sidenav.component';
 
 @Component({
   selector: 'app-view-view',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterModule, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, CustomSidenavComponent, MatBadgeModule, MatMenuModule, MatListModule],
   templateUrl: './view-view.component.html',
   styleUrl: './view-view.component.css'
 })

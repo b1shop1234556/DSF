29e2e1937946b3a9d8c5979df28b24a9d887f558
Dionece12345 <160378010+Dionece12345@users.noreply.github.com>
2024-08-28 19:09:32 +0800

    view-details

--- src/app/Modules/Enrollees/enrollees.routes.ts
+++ src/app/Modules/Enrollees/enrollees.routes.ts
@@ -4,6 +4,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { ListpageComponent } from './listpage/listpage.component';
 import { ViewViewComponent } from './view-view/view-view.component';
 import { RosteringComponent } from './rostering/rostering.component';
+import { ViewDetailsComponent } from './view-details/view-details.component';
 
 export const enrolleeshome: Routes = [
     {path: 'homepage', component: HomepageComponent,
@@ -12,6 +13,7 @@ export const enrolleeshome: Routes = [
             {path: 'approve', component: ApprovepageComponent},
             {path: 'roster', component: RosteringComponent},
             {path: 'view', component: ViewViewComponent},
+            {path: 'viewdetails', component: ViewDetailsComponent},
             // {path: 'newacc/:uid', component: NewaccComponent},
             {path: '', redirectTo: 'list', pathMatch: 'full'}
         ]
--- src/app/Modules/Enrollees/view-details/view-details.component.ts
+++ src/app/Modules/Enrollees/view-details/view-details.component.ts
@@ -3,7 +3,6 @@ import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-// import Swal from 'sweetalert2';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -11,12 +10,12 @@ import { RouterLink, RouterModule } from '@angular/router';
 import { CustomSidenavComponent } from '../../../custom-sidenav/custom-sidenav.component';
 
 @Component({
-  selector: 'app-view-view',
+  selector: 'app-view-details',
   standalone: true,
   imports: [RouterLink, RouterModule, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, CustomSidenavComponent, MatBadgeModule, MatMenuModule, MatListModule],
-  templateUrl: './view-view.component.html',
-  styleUrl: './view-view.component.css'
+  templateUrl: './view-details.component.html',
+  styleUrl: './view-details.component.css'
 })
-export class ViewViewComponent {
+export class ViewDetailsComponent {
 
 }
